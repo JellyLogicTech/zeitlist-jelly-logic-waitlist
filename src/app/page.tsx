c2c87b7fe442d49f6d409381d65eb876f611c000
JellyLogicTech <shaminder.galla@jellylogic.ai>
2025-09-15 21:18:56 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
     <div className="flex flex-col h-screen justify-center items-center text-center">
       <div className="mb-8">
         <SplitText className="text-5xl tracking-tighter font-medium">
-          Get early access
+          Not Sure Where To Start With AI?
         </SplitText>
         <SplitText className="tracking-tight text-xl">
-          Be amongst the first to experience our services.
+          Join Our Waitlist For Early Access
         </SplitText>
       </div>
       <WaitlistForm />
